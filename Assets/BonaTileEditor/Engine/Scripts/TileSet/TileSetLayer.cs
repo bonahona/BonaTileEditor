009b2d473688d13e8f2625a0a098af575dcd6e5a
Björn Fyrvall <bjorn.fyrvall@gmail.com>
2016-11-08 11:17:24 +0100

    Added some "nudge" values to the tileset UVs to avoid weird glitching at the edges.

--- Assets/BonaTileEditor/Engine/Scripts/TileSet/TileSetLayer.cs
+++ Assets/BonaTileEditor/Engine/Scripts/TileSet/TileSetLayer.cs
@@ -101,7 +101,8 @@ public class TileSetLayer
         uvOffsetSize.x = 1.0f / TileSetWidth;
         uvOffsetSize.y = 1.0f / TileSetHeight;
 
-        var uvOffsetNudge = uvOffsetSize * 0;
+        var uvOffsetNudge = uvOffsetSize * 0.05f;
+        Debug.Log(uvOffsetNudge);
 
         for (int y = 0; y < TileSetHeight; y++) {
             for (int x = 0; x < TileSetWidth; x++) {
